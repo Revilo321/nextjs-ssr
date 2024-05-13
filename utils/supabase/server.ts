db0985df9894f16bf71a41ca3573eb2e750718e1
Revilo321 <oliverwii@live.dk>
2024-05-13 23:28:35 +0200

    initial commit

---
+++ utils/supabase/server.ts
@@ -0,0 +1,28 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export function createClient() {
+  const cookieStore = cookies()
+
+  return createServerClient(
+    'https://jyloglvpdokiqgpjxnoi.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_API_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options })
+          } catch (error) {}
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: '', ...options })
+          } catch (error) {}
+        },
+      },
+    }
+  )
+}
