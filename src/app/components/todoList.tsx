bc10426b017ba41791f3c8e2f16f786d9af1eb29
Revilo321 <oliverwii@live.dk>
2024-05-27 13:12:00 +0200

    readded pagination, removed header, for test purposes

--- src/app/components/todoList.tsx
+++ src/app/components/todoList.tsx
@@ -8,12 +8,18 @@ export const TodoList = ({
 }: {
   todos: [{ id: number; title: string; description: string }]
 }) => {
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const itemsPerPage = 10
+  const indexOfLastTodo = currentPage * itemsPerPage
+  const indexOfFirstTodo = indexOfLastTodo - itemsPerPage
+  const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo)
+
   return (
     <div className='container mx-auto px-4 text-black'>
       <div className='bg-white shadow-md rounded-lg p-6'>
         <h1 className='text-2xl font-bold text-gray-700'>Todos</h1>
         <ul className='space-y-4 mt-4'>
-          {todos.map((todo) => (
+          {currentTodos.map((todo) => (
             <li key={todo.id} className='bg-gray-100 p-4 rounded-lg shadow'>
               <h3 className='font-semibold text-lg'>{todo.title}</h3>
               <p className='text-gray-600'>{todo.description}</p>
@@ -25,6 +31,12 @@ export const TodoList = ({
             </li>
           ))}
         </ul>
+        <Pagination
+          totalItems={todos.length}
+          itemsPerPage={itemsPerPage}
+          currentPage={currentPage}
+          onPageChange={setCurrentPage}
+        />
       </div>
     </div>
   )
