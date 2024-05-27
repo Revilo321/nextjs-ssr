e6b4c3722707286604e699a1284472d849cacfce
Revilo321 <oliverwii@live.dk>
2024-05-27 13:21:08 +0200

    added prefetch false

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,21 +15,8 @@ export default async function Home() {
 
   return (
     <div className='min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12'>
-      <div className='relative py-3 sm:max-w-xl sm:mx-auto'>
-        <div className='absolute inset-0 bg-gradient-to-r from-teal-400 to-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl'></div>
-        <div className='relative px-4 py-10 bg-white text-black shadow-lg sm:rounded-3xl sm:p-20'>
-          <h1 className='text-2xl font-semibold'>Todo List</h1>
-          <ul className='mt-6'>
-            {data!.map((todo: any) => (
-              <li
-                key={todo.id}
-                className='mb-4 p-4 bg-gray-100 rounded-lg shadow'>
-                <h2 className='text-xl font-bold'>{todo.title}</h2>
-                <p className='mt-2 text-gray-600'>{todo.description}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className='relative py-3'>
+        <TodoList todos={data} />
       </div>
     </div>
   )
